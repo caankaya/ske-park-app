695809096b12329458a9ac1018aee88697dbad37
can_kaya <info.canoral@gmail.com>
2024-09-09 10:54:19 +0200

    buil :package: Install redux & react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />} errorElement={""}>
+      <Route errorElement={""}>
+        <Route index element={<App />} />
+        {/* Compléter le router avec d'autres pages à partir de là*/}
+        {/* Oubliez pas d'ajouter Outlet dans le composant Layout*/}
+      </Route>
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
