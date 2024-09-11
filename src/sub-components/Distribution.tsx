832d96b12ca6c0e52bd01ef194f29c99ede6f087
can_kaya <info.canoral@gmail.com>
2024-09-11 18:40:56 +0200

    feat(distribution): :sparkles: Dynamisation of vehicles distributions

--- src/sub-components/Distribution.tsx
+++ src/sub-components/Distribution.tsx
@@ -1,10 +1,10 @@
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useAppSelector } from "../redux/types";
+import { IVehicle } from "../@types/vehicle";
 
 export default function Distribution() {
-  const vehicles = useAppSelector((state) => state.vehicles.all);
-  console.log("vehicles :", vehicles);
+  const vehicles: IVehicle = useAppSelector((state) => state.vehicles.all);
   return (
     <div className="flex w-full flex-col items-center justify-start gap-y-5 rounded-xl bg-secondary p-3 laptop:w-1/2">
       <p className="text-sm">Répartition des places</p>
@@ -14,14 +14,22 @@ export default function Distribution() {
             icon={"fa-solid fa-car-side" as IconProp}
             className="size-5 rounded-full bg-info p-2 text-secondary"
           />
-          <span className="place-content-center text-center">45</span>
+          {vehicles && (
+            <span className="place-content-center text-center">
+              {vehicles.cars.length ? vehicles.cars.length : 0}
+            </span>
+          )}
         </div>
         <div className="flex flex-col">
           <FontAwesomeIcon
             icon={"fa-solid fa-motorcycle" as IconProp}
             className="size-5 rounded-full bg-info p-2 text-secondary"
           />
-          <span className="place-content-center text-center">45</span>
+          {vehicles && (
+            <span className="place-content-center text-center">
+              {vehicles.motors.length ? vehicles.motors.length : 0}
+            </span>
+          )}
         </div>
       </div>
     </div>
