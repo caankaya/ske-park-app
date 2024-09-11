1581f2f783c37c0ac9e39d4f978286564096216f
can_kaya <info.canoral@gmail.com>
2024-09-11 11:54:10 +0200

    build: :package: Install & Setup Axios

---
+++ src/axios/index.ts
@@ -0,0 +1,14 @@
+// src/api/axiosInstance.js
+
+import axios from "axios";
+
+// Créer une instance d'axios avec la configuration de base
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL, // La base URL de l'API
+  timeout: 5000, // Temps d'attente maximal
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default instance;
